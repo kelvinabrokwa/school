72695c94c9a4f348d68d4e294e8783b94b41742b
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-12-02 15:42:06 -0500

    finish proj4

--- cs241/proj4/AVL_Tree.py
+++ cs241/proj4/AVL_Tree.py
@@ -12,37 +12,43 @@ class AVL_Tree:
       self._left = None
       self._right = None
 
+
   def __init__(self):
     self._root = None
+
 
   def insert_element(self, value):
     # Insert the value specified into the tree, ensuring that
     # the tree remains balanced after the operation. Your solution
     # must be recursive. This will involve the introduction of
     # additional private methods.
-    self._root = self._balance(self._insert_element_at(self._root, value))
+    self._root = self._insert_element_at(self._root, value)
+
 
   def _insert_element_at(self, node, value):
     if node == None:
       return self._AVL_Node(value)
     elif value < node._value:
       node._left = self._balance(self._insert_element_at(node._left, value))
-      return node
+      return self._balance(node)
     elif value > node._value:
       node._right = self._balance(self._insert_element_at(node._right, value))
+      return self._balance(node)
+    else:
       return node
+
 
   def remove_element(self, value):
     # Remove the value specified from the tree, ensuring that
     # the tree remains balanced after the operation. Your solution
     # must be recursive. This will involve the introduction of
     # additional private methods.
-    self._root = self._balance(self._remove_from(self._root, value))
+    self._root = self._remove_from(self._root, value)
 
 
   def _remove_from(self, node, value):
     if node == None:
-      return None
+      return node
     elif node._value == value: # found the value
       if node._left == None and node._right == None: # no children of node to remove
         return None
@@ -68,25 +74,13 @@ class AVL_Tree:
     else:
       return self._get_min(node._left)
 
+
   def _remove_min(self, node):
     if node._left == None:
       return node._right
     else:
       node._left = self._remove_min(node._left)
       return node
-
-
-  def _find(self, value):
-    curr = self._root
-    while True:
-      if curr == None:
-        return None
-      elif value == curr._value:
-        return curr
-      elif value < curr._value:
-        curr = curr._left
-      elif value > curr._value:
-        curr = curr._right
 
 
   def _balance(self, node):
@@ -101,8 +95,6 @@ class AVL_Tree:
       else:                                   # left-right
         node._left = self._rotate_left(node._left)
         return self._rotate_right(node)
-    elif self._get_balance(node) == 2:
-      pass
     elif balance == 2:
       if self._get_balance(node._right) == 1: # right-right
         return self._rotate_left(node)
@@ -110,11 +102,13 @@ class AVL_Tree:
         node._right = self._rotate_right(node._right)
         return self._rotate_left(node)
 
+
   def _rotate_right(self, node):
     l = node._left
     node._left = l._right
     l._right = node
     return l
+
 
   def _rotate_left(self, node):
     r = node._right
@@ -122,8 +116,10 @@ class AVL_Tree:
     r._left = node
     return r
 
+
   def _get_balance(self, node):
     return self._get_height(node._right) - self._get_height(node._left)
+
 
   def in_order(self):
     # Construct and return a string representing the in-order
@@ -132,7 +128,8 @@ class AVL_Tree:
     # more than one value should be printed as [ 4, 7 ]. Note the spacing.
     result = []
     self._in_order(self._root, result)
-    return '[ ' + ''.join([ str(x) + ', ' for x in result  ]) + ']'
+    return self._stringify(result)
+
 
   def _in_order(self, node, result):
     if node == None:
@@ -146,6 +143,7 @@ class AVL_Tree:
     if node._right != None:
       self._in_order(node._right, result)
 
+
   def pre_order(self):
     # Construct and return a string representing the post-order
     # traversal of the tree. Empty trees should be printed as [ ].
@@ -153,7 +151,8 @@ class AVL_Tree:
     # more than one value should be printed as [ 4, 7 ]. Note the spacing.
     result = []
     self._pre_order(self._root, result)
-    return '[ ' + ''.join([ str(x) + ', ' for x in result  ]) + ']'
+    return self._stringify(result)
+
 
   def _pre_order(self, node, result):
 
@@ -168,6 +167,7 @@ class AVL_Tree:
     if node._right != None:
       self._pre_order(node._right, result)
 
+
   def post_order(self):
     # Construct and return a string representing the pre-order
     # traversal of the tree. Empty trees should be printed as [ ].
@@ -175,7 +175,8 @@ class AVL_Tree:
     # more than one value should be printed as [ 4, 7 ]. Note the spacing.
     result = []
     self._post_order(self._root, result)
-    return '[ ' + ''.join([ str(x) + ', ' for x in result  ]) + ']'
+    return self._stringify(result)
+
 
   def _post_order(self, node, result):
 
@@ -190,29 +191,140 @@ class AVL_Tree:
 
     result.append(node._value)
 
+
+  def _stringify(self, li):
+    output = '[ '
+    for i, val in enumerate(li):
+      output += str(val) + ( ', ' if i != len(li) - 1 else ' ' )
+    output += ']'
+    return output
+
+
   def get_height(self):
     # return an integer that represents the height of the tree.
     # assume that an empty tree has height 0 and a tree with one
     # node has height 1.
     return self._get_height(self._root)
 
+
   def _get_height(self, node):
     if node == None:
       return 0
     return 1 + max(self._get_height(node._left), self._get_height(node._right))
+
 
   def __str__(self):
     return self.in_order()
 
 if __name__ == '__main__':
   t = AVL_Tree()
-  for i in range(5, 0, -1):
+
+  # Test empty tree formatting
+  if t.in_order() != '[ ]':
+    print('FAILED: empty string formatting for in order incorrect')
+  if t.pre_order() != '[ ]':
+    print('FAILED: empty string formatting for pre order incorrect')
+  if t.post_order() != '[ ]':
+    print('FAILED: empty string formatting for post order incorrect')
+
+  # Test insertions, heights and string formatting
+  t.insert_element(1)
+  if t.pre_order() != '[ 1 ]':
+    print('FAILED: incorrect pre order after insertion -> 1')
+  if t.post_order() != '[ 1 ]':
+    print('FAILED: incorrect post order after insertion -> 1')
+  if t.get_height() != 1:
+    print('FAILED: incorrect height')
+
+  t.insert_element(2)
+  if t.pre_order() != '[ 1, 2 ]':
+    print('FAILED: incorrect pre order after insertion -> 2')
+  if t.post_order() != '[ 2, 1 ]':
+    print('FAILED: incorrect post order after insertion -> 2')
+  if t.get_height() != 2:
+    print('FAILED: incorrect height')
+
+  t.insert_element(3)
+  if t.pre_order() != '[ 2, 1, 3 ]':
+    print('FAILED: incorrect pre order after insertion -> 3')
+  if t.post_order() != '[ 1, 3, 2 ]':
+    print('FAILED: incorrect post order after insertion -> 3')
+  if t.get_height() != 2:
+    print('FAILED: incorrect height')
+
+  t.insert_element(4)
+  if t.pre_order() != '[ 2, 1, 3, 4 ]':
+    print('FAILED: incorrect pre order after insertion -> 4')
+  if t.post_order() != '[ 1, 4, 3, 2 ]':
+    print('FAILED: incorrect post order after insertion -> 4')
+  if t.get_height() != 3:
+    print('FAILED: incorrect height')
+
+  t.insert_element(5)
+  if t.pre_order() != '[ 2, 1, 4, 3, 5 ]':
+    print('FAILED: incorrect pre order after insertion -> 5')
+  if t.post_order() != '[ 1, 3, 5, 4, 2 ]':
+    print('FAILED: incorrect post order after insertion -> 5')
+  if t.get_height() != 3:
+    print('FAILED: incorrect height')
+
+  if t.in_order() != '[ 1, 2, 3, 4, 5 ]':
+    print('FAILED: incorrect in order')
+
+  # Insert element already in tree
+  t.insert_element(5)
+  if t.pre_order() != '[ 2, 1, 4, 3, 5 ]':
+    print('FAILED: incorrect pre order after insertion of existing value-> 5')
+  if t.post_order() != '[ 1, 3, 5, 4, 2 ]':
+    print('FAILED: incorrect post order after insertion of existing value -> 5')
+  if t.get_height() != 3:
+    print('FAILED: incorrect height')
+
+
+  # Remove element that is not in the tree
+  try:
+    t.remove_element(10)
+  except:
+    print('FAILED: removing element not in tree threw')
+  if t.pre_order() != '[ 2, 1, 4, 3, 5 ]':
+    print('FAILED: incorrect pre order after insertion of existing value-> 5')
+  if t.post_order() != '[ 1, 3, 5, 4, 2 ]':
+    print('FAILED: incorrect post order after insertion of existing value -> 5')
+  if t.get_height() != 3:
+    print('FAILED: incorrect height')
+
+
+  # Test all deletion types
+  t = AVL_Tree()
+
+  for i in range(1, 9):
     t.insert_element(i)
-  print(t.post_order())
-  t.remove_element(5)
-  print(t.post_order())
+  t.insert_element(0)
+
+  # Delete node with one right child
+  t.remove_element(7)
+  if t.pre_order() != '[ 4, 2, 1, 0, 3, 6, 5, 8 ]':
+    print('FAILED: incorrect pre order after deletion')
+  if t.post_order() != '[ 0, 1, 3, 2, 5, 8, 6, 4 ]':
+    print('FAILED: incorrect post order after deletion')
+
+  # Delete node with one left child
   t.remove_element(1)
-  print(t.post_order())
+  if t.pre_order() != '[ 4, 2, 0, 3, 6, 5, 8 ]':
+    print('FAILED: incorrect pre order after deletion')
+  if t.post_order() != '[ 0, 3, 2, 5, 8, 6, 4 ]':
+    print('FAILED: incorrect post order after deletion')
 
+  # Delete node with two children
+  t.remove_element(2)
+  if t.pre_order() != '[ 4, 3, 0, 6, 5, 8 ]':
+    print("FAILED: incorrect pre order after deletion")
+  if t.post_order() != '[ 0, 3, 5, 8, 6, 4 ]':
+    print('FAILED: incorrect post order after deletion')
 
-
+  # Delete leaf node
+  t.remove_element(5)
+  if t.pre_order() != '[ 4, 3, 0, 6, 8 ]':
+    print('FAILED: incorrect pre order after deletion')
+  if t.post_order() != '[ 0, 3, 8, 6, 4 ]':
+    print('FAILED: incorrect post order after deletion')
