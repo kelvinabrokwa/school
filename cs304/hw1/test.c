7497dd7e3344f94a69c02c74879c5fff3a347092
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-03-14 16:29:18 -0400

    continue hw1

--- cs304/hw1/test.c
+++ cs304/hw1/test.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <limits.h>
-
-typedef unsigned packed_t;
-
-int xbyte(packed_t word, int bytenum) {
-    word = word << ((3 - bytenum) << 3);
-    int m = (1 << 31) & word;
-    word = word >> 24;
-    return word - ((m << 31) >> 24);
-}
 
 int main() {
-    packed_t w = 0xFF000000;
-    int a = xbyte(w, 3);
-    printf("%lu\n", sizeof(a));
+    printf("%f\n", 0.9990234375);
 }
